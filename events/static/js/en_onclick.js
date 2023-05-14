229599261f0cf723d80adff4377755a5f387e818
BaptL02 <94184840+BaptL02@users.noreply.github.com>
2023-05-14 23:41:55 +0200

    Add files via upload

--- events/static/js/en_onclick.js
+++ events/static/js/en_onclick.js
@@ -1,3 +1,4 @@
+<script type="application/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 $(document).ready(function() {
     $('.bouton-filtre').click(function() {
         $.ajax({
