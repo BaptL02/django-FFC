5f43cbbf2ec6c9f6e8dc549e880ee3fa159f4954
BaptL02 <94184840+BaptL02@users.noreply.github.com>
2023-05-14 11:08:48 +0200

    add filter js

---
+++ events/static/js/en_onclick.js
@@ -0,0 +1,12 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+$(document).ready(function() {
+    $('.bouton-filtre').click(function() {
+        $.ajax({
+            url: "{% url 'en_livrees_MSFS' %}?filtre=true",
+            type: "GET",
+            success: function(response) {
+              }
+          });
+      });
+  });
+
