7df7e2c63c3645b9dba09ce76ec6d359b3d06a6b
BaptL02 <94184840+BaptL02@users.noreply.github.com>
2023-05-18 19:21:18 +0200

    Fix

--- events/static/js/en_onclick.js
+++ events/static/js/en_onclick.js
@@ -1,4 +1,3 @@
-<script type="application/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 $(document).ready(function() {
     $('.bouton-filtre').click(function() {
         $.ajax({
