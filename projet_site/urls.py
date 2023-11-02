cab1d93bcbcd52dfe64c868c0017dde02103b261
BaptL02 <94184840+BaptL02@users.noreply.github.com>
2023-11-02 16:59:09 +0100

    Add files via upload

--- projet_site/urls.py
+++ projet_site/urls.py
@@ -4,35 +4,35 @@ from django.conf.urls.static import static
 
 from django.urls import path
 
-#import FRANCAIS
+# import FRANCAIS
 from events.views import events
 from events.views import index
 from events.views import docs
 from events.views import contact
 from events.views import livrees_MSFS
-from events.views import livrees
 from events.views import livrees_XP
 from events.views import voltige
 from events.views import formation_list
 from events.views import privacy_policy
 from events.views import vmr_list
 from events.views import stats
+from events.views import WF
 
-#import ANGLAIS
+# import ANGLAIS
 from events.views import en_index
 from events.views import en_contact
 from events.views import en_docs
-from events.views import en_livrees
 from events.views import en_livrees_XP
 from events.views import en_vmr_list
 from events.views import en_voltige
 from events.views import en_events
 from events.views import en_livrees_MSFS
 from events.views import en_formation_list
+from events.views import en_WF
 
 urlpatterns = [
 
-    #Lien FRANCAIS
+    # Lien FRANCAIS
     path('admin/', admin.site.urls),
     path('', index, name="index"),
     path('index/', index, name="index"),
@@ -41,22 +41,23 @@ urlpatterns = [
     path('documentation/formation/', formation_list, name="formation_list"),
     path('contact/', contact, name="contact"),
     path('livrees/livrees_MSFS/', livrees_MSFS, name="livrees_MSFS"),
-    path('livrees/', livrees, name="livrees"),
     path('voltige/', voltige, name="voltige"),
     path('livrees_xp/', livrees_XP, name="livrees_XP"),
     path('contact/privacy_policy/', privacy_policy, name="privacy_policy"),
-    path('livrees/vmr/', vmr_list, name="vmr_list" ),
+    path('livrees/vmr/', vmr_list, name="vmr_list"),
     path('stats', stats, name="stats"),
+    path('events/worldflight', WF, name='worldflight'),
 
-    #Lien ANGLAIS
+    # Lien ANGLAIS
     path('en/', en_index, name="en_index"),
     path('en/contact', en_contact, name="en_contact"),
     path('en/documentation', en_docs, name="en_doc"),
-    path('en/livrees', en_livrees, name="en_livrees"),
     path('en/livrees_XP', en_livrees_XP, name="en_livrees_XP"),
     path('en/vmr_list', en_vmr_list, name="en_vmr_list"),
     path('en/voltige', en_voltige, name="en_voltige"),
     path('en/events', en_events, name="en_events"),
-    path('en/documentation/formation/', en_formation_list, name="en_formation_list"),
+    path('en/documentation/formation/',
+         en_formation_list, name="en_formation_list"),
     path('en/livrees/livrees_MSFS/', en_livrees_MSFS, name="en_livrees_MSFS"),
+    path('en/events/worldflight', en_WF, name="en_WF"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
